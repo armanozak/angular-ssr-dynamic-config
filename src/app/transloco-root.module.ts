292d16a21448e5605acc441e25baed7778b9c2a9
Levent Arman Özak <arman@armanozak.com>
2022-03-23 16:43:49 +0100

    Transfer Transloco state too

--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -7,17 +7,34 @@ import {
   translocoConfig,
   TranslocoModule,
 } from '@ngneat/transloco';
-import { Injectable, NgModule } from '@angular/core';
+import { Inject, Injectable, NgModule, PLATFORM_ID } from '@angular/core';
 import { environment } from '../environments/environment';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
+import { of, tap } from 'rxjs';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private state: TransferState,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(
-      `${environment.baseUrl}/assets/i18n/${lang}.json`
-    );
+    const key = makeStateKey<Translation>(`transloco-${lang}`);
+
+    if (this.state.hasKey(key)) return of(this.state.get(key, {}));
+
+    return this.http
+      .get<Translation>(`${environment.baseUrl}/assets/i18n/${lang}.json`)
+      .pipe(
+        tap((json) => {
+          if (isPlatformServer(this.platformId)) {
+            this.state.set(key, json);
+          }
+        })
+      );
   }
 }
 
